50a2e6da588dd7bd8b9963028d590cc808e2ed18
huangzijian <huangzijian@lamdaer.com>
2020-10-17 10:52:22 +0800

    feat(blinkapplication): 添加mapper扫描

--- src/main/java/com/lamdaer/blink/BlinkApplication.java
+++ src/main/java/com/lamdaer/blink/BlinkApplication.java
@@ -1,9 +1,14 @@
 package com.lamdaer.blink;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author lamdaer
+ */
 @SpringBootApplication
+@MapperScan(basePackages = "com.lamdaer.blink.mapper")
 public class BlinkApplication {
     
     public static void main(String[] args) {
