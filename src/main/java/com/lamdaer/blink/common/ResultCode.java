ee1e447648d9bb1ceaf06803d2fe81f440ac5dd0
huangzijian <huangzijian@lamdaer.com>
2020-10-17 10:13:03 +0800

    feat(common): 添加统一结果类

---
+++ src/main/java/com/lamdaer/blink/common/ResultCode.java
@@ -0,0 +1,10 @@
+package com.lamdaer.blink.common;
+
+/**
+ * @author lamdaer
+ * @createTime 2020/10/17
+ */
+public interface ResultCode {
+    Integer SUCCESS = 20000;
+    Integer ERROR = 20001;
+}
