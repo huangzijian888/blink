1fcdcc2d39826911cb91be8fcf4496456e312827
huangzijian <huangzijian@lamdaer.com>
2020-10-17 10:09:32 +0800

    feat(handler): 新增自动填充配置

---
+++ src/main/java/com/lamdaer/blink/handler/MyMetaObjectHandler.java
@@ -0,0 +1,31 @@
+package com.lamdaer.blink.handler;
+
+import java.util.Date;
+
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author lamdaer
+ * @createTime 2020/10/17
+ */
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("start insert fill ...." + new Date());
+        this.strictInsertFill(metaObject, "createTime", Date.class, new Date());
+        this.strictInsertFill(metaObject, "updateTime", Date.class, new Date());
+    }
+    
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("start update fill ....");
+        this.strictUpdateFill(metaObject, "updateTime", Date.class, new Date());
+    }
+}
